67906a2b19611baa10ec2c46815d67e98f88edaf
尤雨东 <root@ltpp.vip>
2025-05-11 12:25:06 +0800

    feat: 301

--- src/app/controller/root/fn.rs
+++ src/app/controller/root/fn.rs
@@ -1,33 +1,42 @@
 use super::*;
 
 pub async fn handle(ctx: Context) {
-    let dir: String = ctx.get_route_param(DIR_KEY).await.unwrap_or_default();
-    let file: String = ctx.get_route_param(FILE_KEY).await.unwrap_or_default();
-    if dir.is_empty() {
+    let dir_name: String = ctx.get_route_param(DIR_KEY).await.unwrap_or_default();
+    let file_name: String = ctx.get_route_param(FILE_KEY).await.unwrap_or_default();
+    if dir_name == INDEX_HTML_FILE_NAME {
+        ctx.set_response_status_code(301)
+            .await
+            .set_response_header(LOCATION, INDEX_HTML_URL_PATH)
+            .await
+            .set_response_body(vec![])
+            .await;
+        return;
+    }
+    if dir_name.is_empty() {
         ctx.set_response_status_code(200)
             .await
-            .set_response_body(ROOT_HTML)
+            .set_response_body(INDEX_HTML)
             .await
             .set_response_header(CONTENT_TYPE, TEXT_HTML)
             .await;
         return;
     }
-    let file_path: String = if file.is_empty() {
-        format!("./group-chat/dist/{dir}")
+    let file_path: String = if file_name.is_empty() {
+        format!("./group-chat/dist/{dir_name}")
     } else {
-        format!("./group-chat/dist/{dir}/{file}")
+        format!("./group-chat/dist/{dir_name}/{file_name}")
     };
-    let extension_name: String = if file.is_empty() {
-        FileExtension::get_extension_name(&file)
-    } else {
-        FileExtension::get_extension_name(&dir)
-    };
+    let extension_name: String = FileExtension::get_extension_name(&file_path);
     let content_type: &str = FileExtension::parse(&extension_name).get_content_type();
-    let res: Vec<u8> = async_read_from_file(&file_path).await.unwrap();
+    let res: Option<Vec<u8>> = async_read_from_file(&file_path).await.ok();
+    if res.is_none() {
+        return;
+    }
+    let body: Vec<u8> = res.unwrap_or_default();
     ctx.set_response_status_code(200)
         .await
         .set_response_header(CONTENT_TYPE, content_type)
         .await
-        .set_response_body(res)
+        .set_response_body(body)
         .await;
 }
--- src/config/business/root/const.rs
+++ src/config/business/root/const.rs
@@ -1,3 +1,5 @@
 pub const DIR_KEY: &str = "dir";
 pub const FILE_KEY: &str = "file";
-pub const ROOT_HTML: &str = include_str!("../../../../group-chat/dist/index.html");
+pub const INDEX_HTML: &str = include_str!("../../../../group-chat/dist/index.html");
+pub const INDEX_HTML_FILE_NAME: &str = "index.html";
+pub const INDEX_HTML_URL_PATH: &str = "/";
